e49adfd910bf0684ed1616368fe916e3b75d34e1
git <jfoulds00@gmail.com>
2021-07-23 11:42:55 -0400

    fixed main.py

--- common/tools.py
+++ common/tools.py
@@ -1,14 +1,7 @@
 import threading
-from comm import comm
+
 lock = threading.Lock()
 
-ns = {}
-
-ns['serverAdr'] = '10.6.1.26'
-ns['serverPort'] = 5001
-ns['listenPort'] = 5002
-ns['serverMessageHandlerRunning'] = True
-com = comm(Inet=ns['serverAdr'],send_port=ns['serverPort'],listen_port=ns['listenPort'])
 
 def lprint(*a,**b):
     with lock:
--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from common.tools import *
 
 
 #manager = multiprocessing.Manager()
+ns = {}
+
+ns['serverAdr'] = '10.6.1.26'
+ns['serverPort'] = 5001
+ns['listenPort'] = 5002
+ns['serverMessageHandlerRunning'] = True
+com = comm(Inet=ns['serverAdr'],send_port=ns['serverPort'],listen_port=ns['listenPort'])
 
 
 
